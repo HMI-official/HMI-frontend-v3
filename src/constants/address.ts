a130835e8341871ff7ca7d450732a63e86a1e9a0
abehan7 <hanjk13262@gmail.com>
2022-07-09 10:09:11 +0900

    add:> tiktok address

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -2,6 +2,6 @@ export const ADDRESS = {
   discord: "https://discord.gg/XAknqH2xu8",
   instagram: "https://www.instagram.com/hiplanet_nft/",
   twitter: "https://twitter.com/HIPLANET_NFT",
-  tiktok: "",
+  tiktok: "https://www.tiktok.com/@highmindedintelligence",
   hmi: "https://highmindedi.com/",
 };
