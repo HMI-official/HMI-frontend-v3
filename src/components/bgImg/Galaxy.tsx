36e0fe24c095b8af0a3282609b647b56c274dd5a
abehan7 <hanjk13262@gmail.com>
2022-07-09 14:03:04 +0900

    chore:> bg mobile

--- src/components/bgImg/Galaxy.tsx
+++ src/components/bgImg/Galaxy.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { ETC_IMAGES } from "../../constants/image";
+import { media } from "../../styles/Themes";
 
 const Galaxy = () => {
   return (
@@ -26,6 +27,20 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+  ${media.mobile} {
+    height: 100%;
+    /* background-image: url(${ETC_IMAGES.bg2}); */
+
+    /* background-image size to 50% */
+    /* background-size: 40%; */
+    /* background-size: cover; */
+    /* background-position: center; */
+    /* background-repeat: no-repeat; */
+
+    /* width: 40rem; */
+    /* top: 50%; */
+    /* opacity: 1; */
   }
 `;
 const Overlay = styled.div`
