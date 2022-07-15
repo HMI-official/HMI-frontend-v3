ed9f2985b9f805f380e696826251f0cd0f514a1b
abehan7 <hanjk13262@gmail.com>
2022-07-15 09:26:33 +0900

    chore:> reset web3config

--- src/web3Config.tsx
+++ src/web3Config.tsx
@@ -938,13 +938,236 @@ export const MINT_NFT_ABI: AbiItem[] = [
   },
 ];
 
+export const SALE_NFT_ABI: AbiItem[] = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_mintGemToken",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getOnSaleGemToken",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getTokenPrice",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "mintGemToken",
+    outputs: [
+      {
+        internalType: "contract MintGemToken",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "onSaleGemToken",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "purchaseGemToken",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_price",
+        type: "uint256",
+      },
+    ],
+    name: "setForSaleGemToken",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "tokenPrices",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const GET_NFT_ABI: AbiItem[] = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_gms",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_tokenOwner",
+        type: "address",
+      },
+    ],
+    name: "getLatestToken",
+    outputs: [
+      {
+        internalType: "uint256[2]",
+        name: "",
+        type: "uint256[2]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_tokenOwner",
+        type: "address",
+      },
+    ],
+    name: "getMyNfts",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_tokenOwner",
+        type: "address",
+      },
+    ],
+    name: "getUserTokenLength",
+    outputs: [
+      {
+        internalType: "uint256[2]",
+        name: "",
+        type: "uint256[2]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "gms",
+    outputs: [
+      {
+        internalType: "contract GMS",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
 export const MINT_NFT_ADDRESS = "0xdee7F21D02adB2780f0238Da5263796cFe043657";
+
+export const SALE_NFT_ADDRESS = "0x5958A71b0607Cde827864f7b4A4428308EfD49DB";
+
+export const GET_NFT_ADDRESS = "0x9cff75736dfE38AFC4963CAd12BE4b7a1240894E";
 
 export const MERKEL_ROOT =
   "0x74f4666169faccda89a45d47ab1997a62f24c3cd534a01539db8f0e40d3eb8b1";
 export const config = {
   MINT_NFT_ABI,
+  GET_NFT_ABI,
   MINT_NFT_ADDRESS,
+  GET_NFT_ADDRESS,
   maxMintAmount: 10,
   presaleMaxMintAmount: 3,
   price: 0.01,
