19bcda7e6e5b2a88904d416c9054a3dcaf3196a1
abehan7 <hanjk13262@gmail.com>
2022-08-08 13:00:35 +0900

    chore:>

--- src/config/crossmint.ts
+++ src/config/crossmint.ts
@@ -1,24 +1,22 @@
-export const publicCrossmintConfig = {
+const common = {
   collectionTitle: "HMI",
-  collectionDescription: "HI-PLANET(public-mint)",
   collectionPhoto:
     "https://media.discordapp.net/attachments/971049082908971008/992299198487744522/gif.gif?width=609&height=609",
-  clientId: "d54db2c5-8dd2-492c-907e-c066f746fef8",
   environment: "staging",
+};
+
+export const publicCrossmintConfig = {
+  collectionTitle: common.collectionTitle,
+  collectionDescription: "HI-PLANET(public-mint)",
+  collectionPhoto: common.collectionPhoto,
+  clientId: "d54db2c5-8dd2-492c-907e-c066f746fef8",
+  environment: common.environment,
 };
 
 export const wlCrossmintConfig = {
-  collectionTitle: "HMI",
+  collectionTitle: common.collectionTitle,
   collectionDescription: "HI-PLANET(wl-mint)",
-  collectionPhoto:
-    "https://media.discordapp.net/attachments/971049082908971008/992299198487744522/gif.gif?width=609&height=609",
+  collectionPhoto: common.collectionPhoto,
   clientId: "d567ce5c-6d27-4fcc-b213-13a78c4b811d",
-  environment: "staging",
-};
-
-export const commonCrossmintConfig = {
-  collectionTitle: "HMI",
-  collectionPhoto:
-    "https://media.discordapp.net/attachments/971049082908971008/992299198487744522/gif.gif?width=609&height=609",
-  environment: "staging",
+  environment: common.environment,
 };
